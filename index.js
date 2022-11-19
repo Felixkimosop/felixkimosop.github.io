b3e3c1cfcc2106d86be5278ea41b329d24be4752
Felix Kipruto <kimosopfelix@gmail.com>
2022-11-19 13:32:44 +0300

    Added both mouseover and mouseout to the button

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ function save(){
 
     alert ("Downloading")
 }
+
+function down(){
+
+    document.getElementById('down').innerHTML = "download"
+}
+
+function down2(){
+
+    document.getElementById('down').innerHTML="get my resume"
+}
